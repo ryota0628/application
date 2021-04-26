ec401d4d15293ebe172663ddea1a8019d57ebf1f
ryota0628 <ryota_1dan@live.jp>
2021-04-26 22:55:20 +0900

    データ照合完了、山場乗り越えた

--- resources/views/calendar.blade.php
+++ resources/views/calendar.blade.php
@@ -4,18 +4,13 @@
    <div class="row justify-content-center">
        <div class="col-md-8">
            <div class="card">
-               <div class="card-header text-center">
-					<a class="btn btn-outline-secondary float-left" href="{{ url('/?date=' . $calendar->getPreviousMonth()) }}">前の月</a>
-					
-					<span>{{ $calendar->getTitle() }}</span>
-					
-					<a class="btn btn-outline-secondary float-right" href="{{ url('/?date=' . $calendar->getNextMonth()) }}">次の月</a>
-				</div>
-				<div class="card-body">
+               <div class="card-header">{{ $calendar->getTitle() }}</div>
+               <div class="card-body">
 					{!! $calendar->render() !!}
                </div>
            </div>
        </div>
    </div>
 </div>
+<a href='/schedules/register' style="font-size:30pt;font-weight:bold;"><center>[予定登録はこちら]<center></a><!--登録ページへのリンク-->
 @endsection
