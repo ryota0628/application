efc28e0f07600191b14dbe65bb0eb86013288006
ryota0628 <ryota_1dan@live.jp>
2021-05-04 20:48:06 +0900

    完成

--- app/Http/Controllers/CalendarController.php
+++ app/Http/Controllers/CalendarController.php
@@ -12,18 +12,17 @@ class CalendarController extends Controller
 		
 		//dateがYYYY-MMの形式かどうか判定する
 		if($date && preg_match("/^[0-9]{4}-[0-9]{2}$/", $date)){
-			$date = strtotime($date . "-01");
+			$date = strtotime($date . "-02");
 		}else{
 			$date = null;
 		}
-		dd($date);
+		
 		//取得出来ない時は現在(=今月)を指定する
 		if(!$date)$date = time();
-		//dd($date);
 
 		//カレンダーに渡す
 		$calendar = new CalendarView($date);
-        //dd($calendar);
+		
 		return view('calendar', [
 			"calendar" => $calendar
 		]);
